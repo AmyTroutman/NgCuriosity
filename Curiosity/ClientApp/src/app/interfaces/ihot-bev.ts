352dcac15e9eaa5f331336fbc4155febcf833daa
AmyTroutman <amyrtroutman@gmail.com>
2020-10-18 18:01:41 -0500

    Interface and service for HotBev done. Updated readme

---
+++ Curiosity/ClientApp/src/app/interfaces/ihot-bev.ts
@@ -0,0 +1,10 @@
+export interface IHotBev {
+    id?: number;
+    brand: string;
+    name: string;
+    type: string;
+    subtype: string;
+    review: string;
+    mood: string;
+    userId?: string;
+}
--- Curiosity/ClientApp/src/app/services/hot-bev.service.ts
+++ Curiosity/ClientApp/src/app/services/hot-bev.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { IHotBev } from '../interfaces/ihot-bev';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotBevService {
 
-  constructor() { }
+  hotBevs: IHotBev[];
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
+
+  getBevs(): Promise<IHotBev[]> {
+    return this.http.get<IHotBev[]>(`${this.baseUrl}hotbevs`).toPromise();
+  }
+
+  async getBev(id: number): Promise<IHotBev> {
+    return await this.http.get<IHotBev>(`${this.baseUrl}hotbevs/${id}` ).toPromise();
+  }
+
+  addBev(newBev: IHotBev): Promise<IHotBev> {
+    return this.http.post<IHotBev>(`${this.baseUrl}hotbevs`, newBev).toPromise();
+  }
+
+  async updateBev(bevId: number, bev: IHotBev) {
+    return await this.http.put<IHotBev>(`${this.baseUrl}hotbevs/${bevId}`, bev).toPromise();
+  }
+
+  async deleteBev(bevId: number) {
+    return await this.http.delete<IHotBev>(`${this.baseUrl}hotbevs/${bevId}`).toPromise();
+  }
 }
